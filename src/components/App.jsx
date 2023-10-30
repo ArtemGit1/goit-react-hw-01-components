0e8409d21342e69ca9338ada068cbbbdf611b447
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-10-30 13:52:56 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import React from 'react';
+import user from './user.json';
+import Profile from './Profile.js';
+import data from './data.json';
+import Statistics from './Statistics.js';
+import friends from './friends.json';
+import FriendList from './FriendList.js';
+import transactions from './transactions.json';
+import TransactionHistory from './TransactionHistory.js';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="app">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
